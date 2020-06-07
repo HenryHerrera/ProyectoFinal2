328276277c6ceb52be5a75a372a21fcef73a3fba
HenryHerrera <65210842+HenryHerrera@users.noreply.github.com>
2020-06-06 20:44:15 -0600

    Se agregaron botones, tabla y se creo la funcion agregar

--- src/proyectof/ProyectoF.java
+++ src/proyectof/ProyectoF.java
@@ -1,9 +1,11 @@
+package proyectof;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectof;
+
 
 /**
  *
@@ -16,6 +18,10 @@ public class ProyectoF {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        JFrameBase vista = new JFrameBase();
+        vista.setVisible(true);
+        
     }
     
 }
